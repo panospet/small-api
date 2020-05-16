d0634563715fd7ee69a463621c4cbd7c57a4bcf5
panospet <panospet@gmail.com>
2020-05-16 23:08:39 +0300

    use env variables for redis and mysql path

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,14 +1,28 @@
 package config
 
+import (
+	"log"
+	"os"
+)
+
 type Config struct {
 	MysqlPath string
 	RedisPath string
 }
 
-// todo env variable
 func NewConfig() *Config {
+	mysqlPath := os.Getenv("MYSQL_PATH")
+	if mysqlPath == "" {
+		log.Println("Mysql path from env is empty. Using default value")
+		mysqlPath = "bestprice:bestprice@(localhost:3305)/bestprice?parseTime=true"
+	}
+	redisPath := os.Getenv("REDIS_PATH")
+	if redisPath == "" {
+		log.Println("Redis path from env is empty. Using default value")
+		redisPath = "redis://localhost:6380/1"
+	}
 	return &Config{
-		MysqlPath: "bestprice:bestprice@(localhost:3305)/bestprice?parseTime=true",
-		RedisPath: "redis://localhost:6380/1",
+		MysqlPath: mysqlPath,
+		RedisPath: redisPath,
 	}
 }
