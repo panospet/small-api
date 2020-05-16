fba856bf15732fe17084f827dc8d3ecd9741b459
panospet <panospet@gmail.com>
2020-05-16 14:12:55 +0300

    add flagvars to user creation cmd

--- cmd/user-add/main.go
+++ cmd/user-add/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"flag"
+	"log"
+
 	"github.com/panospet/small-api/internal/config"
 	"github.com/panospet/small-api/pkg/model"
 	"github.com/panospet/small-api/pkg/services"
 )
 
 func main() {
+	var username string
+	var password string
+
+	flag.StringVar(&username, "username", "", "admin username")
+	flag.StringVar(&password, "password", "", "admin password")
+	flag.Parse()
+
+	if username == "" || password == "" {
+		log.Fatalln("please give username and password")
+	}
+
 	conf := config.NewConfig()
 	db, err := services.NewDb(conf.MysqlPath)
 	if err != nil {
@@ -14,8 +28,8 @@ func main() {
 	}
 
 	user := model.User{
-		Username:  "admin", // todo username and password using args
-		Password:  "admin",
+		Username: username,
+		Password: password,
 	}
 
 	err = db.AddUser(user)
