365728bfb5ec366c75e7b5c21c0a4c6f1b6838e9
panospet <panospet@gmail.com>
2020-05-17 20:54:23 +0300

    more api unit tests

--- pkg/api/api_test.go
+++ pkg/api/api_test.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/panospet/small-api/pkg/model"
 	"net/http"
 	"net/http/httptest"
@@ -46,12 +45,56 @@ func (s *Suite) TestGetProducts() {
 	handler.ServeHTTP(rr, req)
 
 	assert.Equal(s.T(), http.StatusOK, rr.Code)
-	fmt.Println(rr.Body.String())
 	bodyBytes := []byte(rr.Body.String())
 	var prods []model.Product
 	err = json.Unmarshal(bodyBytes, &prods)
 	assert.Nil(s.T(), err)
 	assert.Len(s.T(), prods, 10)
+}
+
+func (s *Suite) TestGetProductsPerPage() {
+	req, err := http.NewRequest("GET", "/v1/products?perPage=40", nil)
+	assert.Nil(s.T(), err)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(s.api.getListProducts)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(s.T(), http.StatusOK, rr.Code)
+	bodyBytes := []byte(rr.Body.String())
+	var prods []model.Product
+	err = json.Unmarshal(bodyBytes, &prods)
+	assert.Nil(s.T(), err)
+	assert.Len(s.T(), prods, 40)
+}
+
+func (s *Suite) TestGetProductsPerPageWithLimit() {
+	req, err := http.NewRequest("GET", "/v1/products?perPage=40&limit=20", nil)
+	assert.Nil(s.T(), err)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(s.api.getListProducts)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(s.T(), http.StatusOK, rr.Code)
+	bodyBytes := []byte(rr.Body.String())
+	var prods []model.Product
+	err = json.Unmarshal(bodyBytes, &prods)
+	assert.Nil(s.T(), err)
+	assert.Len(s.T(), prods, 20)
+}
+
+func (s *Suite) TestGetCategories() {
+	req, err := http.NewRequest("GET", "/v1/categories", nil)
+	assert.Nil(s.T(), err)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(s.api.getListCategories)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(s.T(), http.StatusOK, rr.Code)
+	bodyBytes := []byte(rr.Body.String())
+	var categories []model.Category
+	err = json.Unmarshal(bodyBytes, &categories)
+	assert.Nil(s.T(), err)
+	assert.Len(s.T(), categories, 10)
 }
 
 func TestSuite(t *testing.T) {
